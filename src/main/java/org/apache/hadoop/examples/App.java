254322752fe0a078879114e2898c963710ce853c
AleVassa06 <alessandro.vassallo@students.uniroma2.eu>
2025-04-03 17:41:31 +0200

    prima versione

---
+++ src/main/java/org/apache/hadoop/examples/App.java
@@ -0,0 +1,15 @@
+package org.apache.hadoop.examples;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+
+    public static void main( String[] args )
+    {
+        Cacca cacca= new Cacca(2);
+        System.out.println( "Hello World!"+ cacca.getDensita() );
+    }
+}
---
+++ src/main/java/org/apache/hadoop/examples/Cacca.java
@@ -0,0 +1,16 @@
+package org.apache.hadoop.examples;
+
+public class Cacca {
+    private int densita;
+    public Cacca(int densita) {
+        this.densita = densita;
+    }
+
+    public int getDensita() {
+        return densita;
+    }
+
+    public void setDensita(int densita) {
+        this.densita = densita;
+    }
+}
